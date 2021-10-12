e901785adaa1e79f0b53cee36cdb34b4ae020b62
SashaKlash <you@example.com>
2021-10-12 16:28:29 +0200

    Aujourd'hui j'ai bien travaillé.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once "config.php";
 // Routeur
 if(isset($_GET['pg'])){
     // chargment de l'accueil
+    include_once "import/homepage.php"
 }else{
     // pas sur l'accueil
     switch($_GET['pg']){
