dbdb7f1cce0fac93647ebe734268087d27b1e8ad
SashaKlash <you@example.com>
2021-10-12 16:15:10 +0200

    configuration du routeur

--- index.php
+++ index.php
@@ -4,4 +4,21 @@
 Contrôleur frontal
 */
 
+// chargment des dépendances
 require_once "config.php";
+
+// Routeur
+if(isset($_GET['pg'])){
+    // chargment de l'accueil
+}else{
+    // pas sur l'accueil
+    switch($_GET['pg']){
+        case "cv":
+            // import de cv
+            echo "cv";
+            break;
+        case "tuto":
+            echo "tuto lulu";
+            break;
+    }
+}
