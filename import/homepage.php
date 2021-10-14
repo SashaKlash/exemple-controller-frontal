3ebdb1abff1e03e9cb0ea3418515cddbbfe60d0d
SashaKlash <you@example.com>
2021-10-14 15:23:14 +0200

    Intégration de la barre de navigation dans la homepage sans passer par index.php

--- import/homepage.php
+++ import/homepage.php
@@ -4,3 +4,18 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Accueil</h1>
+    <?php
+    // On peut imoprter le menu en PHP sans passer par index.php
+    include "menu.php";
+    ?>
+    <img src="img/quoi.jpg" alt="C'est un petit Minion">
+</body>
+</html>
